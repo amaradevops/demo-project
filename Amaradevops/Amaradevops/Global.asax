08611d94f67e81751377c4f2c1eba0508b8e3958
amara <amaradevops1234@gmail.com>
2019-12-26 16:34:42 +0530

    hi

---
+++ Amaradevops/Amaradevops/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Amaradevops.MvcApplication" Language="C#" %>
---
+++ Amaradevops/Amaradevops/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Amaradevops.Startup))]
+namespace Amaradevops
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
